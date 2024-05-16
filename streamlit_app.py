5c8b38a1a42e8bccc1caacea0a5bfced984df9c6
jtmero <86637429+jtmero@users.noreply.github.com>
2024-05-16 11:09:26 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.write(message["role"])
+        st.write(message["content"])
         
 # Accept user input
 if prompt := st.chat_input("What is up?"):
